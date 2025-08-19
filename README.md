cdf1961dcbb917330071d5921b6ba4aa693ac387
leksika1 <iliyaleksin@gmail.com>
2025-08-19 06:12:10 +0300

    add base file

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Pet-project, search for train tickets
 
 ## link to the unidraw board
-https://unidraw.io/app/board/346e8077bf9ac17c3b87
+https://unidraw.io/app/board/346e8077bf9ac17c3b87git pull origin main
---
+++ app/update_data.py
@@ -0,0 +1,6 @@
+"""                                     DOCS
+
+    Collects data about railway tickets from the website and records them in the database           """
+
+
+import requests  #библа для запросов к сайтам
